3be50fcb3e34a2340b4430ba0be8e3066d848a1b
Oli James <olivermarkjames@gmail.com>
2012-09-14 19:23:08 +1200

    carrierwave local done

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,5 +1,5 @@
 class Slide < ActiveRecord::Base
-  attr_accessible :content, :micropost_id, :image, :remote_image_url
+  attr_accessible :content, :micropost_id, :image, :remote_image_url, :image_cache
   belongs_to :micropost
 
   validates :content, :length => { :maximum => 280 }
